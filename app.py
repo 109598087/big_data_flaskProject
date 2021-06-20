ee0558f337adb51359e1130c7b48c3c90e16c0a3
109598087 <t109598087@ntut.org.tw>
2021-06-20 21:04:31 +0800

    amsMenu.html -> memberRegisterRequest.html

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def homepage():
-    return 'Hello, World!aab'
+    if request.method == 'POST':
+        agentName = request.form.get('agentName')
+        choosedButton = request.form.get('choosedButton')
+        if choosedButton == 'memberRegisterBtn':
+            return redirect(url_for("memberRegisterRequest", agentName=agentName))
+        if choosedButton == 'configSettingBtn':
+            return 'configSettingBtn'
+        if choosedButton == 'reportQueryBtn':
+            return 'reportQueryBtn'
+    return render_template('amsMenu.html')
 
 
-@app.route('/test')
-def in_test_page():
-    return 'In test page'
+@app.route('/in_test_page')
+def memberRegisterRequest():
+    # return render_template('memberRegisterRequest.html')
+    agentName = request.args.get('agentName')
+    return render_template('memberRegisterRequest.html', agentName=agentName)
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True, port=5000)
